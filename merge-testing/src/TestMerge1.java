aec0daecf2fe35c3c736a248ec73afb6c4c3eaab
VirSalsus <102230465+VirSalsus@users.noreply.github.com>
2022-04-05 22:04:00 +0200

    Add files via upload

---
+++ merge-testing/src/TestMerge1.java
@@ -0,0 +1,13 @@
+
+public class TestMerge1 {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 2;
+		
+		System.out.println("Maxi war hier");
+
+	}
+
+}
---
+++ merge-testing/src/TestMerge2.java
@@ -0,0 +1,16 @@
+
+public class TestMerge2 {
+
+	public static void main(String[] args) {
+
+		String str1 = "abc";
+		String str2 = "Testing";
+	
+		
+		// 1
+		// 2
+		// 3
+
+	}
+
+}
