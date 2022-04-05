bf6186d5f0d05b2eb34756fe2ac9598de0033e3d
FOehlers <fabian.oehlers@web.de>
2022-04-05 22:55:13 +0200

    ethgedh

--- merge-testing/src/TestMerge1.java
+++ merge-testing/src/TestMerge1.java
@@ -7,7 +7,7 @@ public class TestMerge1 {
 		int b = 2;
 		
 		System.out.println("Fabi war auch hier");
-		System.out.println("Batman");
+		System.out.println("Batman3245632563");
 		System.out.println("sauce");
 		
 		
